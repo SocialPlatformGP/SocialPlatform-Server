224f7ebec4ffa2d097f6b20098e34415a4034ebc
Mohamed Zarea <mohamedzarea29@gmail.com>
2024-03-05 18:30:32 +0200

    init

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,19 @@
+package com.example
+
+import com.example.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureHTTP()
+    configureMonitoring()
+    configureSerialization()
+    configureTemplating()
+    configureDatabases()
+    configureSockets()
+    configureRouting()
+}
---
+++ src/main/kotlin/com/example/plugins/HTTP.kt
@@ -0,0 +1,30 @@
+package com.example.plugins
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.plugins.httpsredirect.*
+import io.ktor.server.plugins.partialcontent.*
+
+fun Application.configureHTTP() {
+    install(CORS) {
+        allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Delete)
+        allowMethod(HttpMethod.Patch)
+        allowHeader(HttpHeaders.Authorization)
+        allowHeader("MyCustomHeader")
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    }
+    install(HttpsRedirect) {
+        // The port to redirect to. By default 443, the default HTTPS port.
+        sslPort = 443
+        // 301 Moved Permanently, or 302 Found redirect.
+        permanentRedirect = true
+    }
+    install(PartialContent) {
+        // Maximum number of ranges that will be accepted from a HTTP request.
+        // If the HTTP request specifies more ranges, they will all be merged into a single range.
+        maxRangeCount = 10
+    }
+}
